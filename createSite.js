ce2361d5d6799cfd7116cece41067b1467fc7fa7
NBurtsev790231 <nburtsev7922@gmail.com>
2023-02-24 23:16:39 +0300

    create architecture site

--- createSite.js
+++ createSite.js
@@ -1,10 +1,17 @@
 'use strict'
-window.onload = function () {
-	setTimeout(function () {
-		document.body.classList.add('loaded');
-		Draggable.create('.gallery', {
-			bounds: 'body',
-			inertia: true
-		})
+window.onload = function() {
+	setTimeout(function() {
+		document.body.classList.add('loaded')
+
+		if (window.matchMedia('(min-width: 992px)').matches) { // Если не смартфон
+
+			Draggable.create('.gallery', { // Запускаем плагин Draggable
+				bounds: 'body',
+				inertia: true
+			})
+			
+		}
+ 
 	}, 200)
+ 
 }
